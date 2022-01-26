6e7dd56f7b1301e8613a9a908c5a1cdf227d7921
Kitt Cheung <kitt.cheung@live.se>
2022-01-26 21:31:28 +0100

    feat: store input to a p-tag

--- src/Components/Views/Login.js
+++ src/Components/Views/Login.js
@@ -3,9 +3,19 @@ import { useState } from "react";
 const Users = () => {
   //const guitars = [];
   const [users, setUsers] = useState([]);
+  const [name, setName] = useState("")
+  
+  const handleInput = event => {
+    setName(event.target.value)
+  }
 
+  async function getUser(name) {
+    const response = await fetch("https://trivia-game-noroff-api.herokuapp.com/trivia");
+    const data = await response.json();
+    return data.find(x => x.username === name);
+}
 
-const handleLoadUserData = async () => {
+  const handleLoadUserData = async () => {
     const response = await fetch(
       "https://trivia-game-noroff-api.herokuapp.com/trivia"
     );
@@ -16,10 +26,12 @@ const handleLoadUserData = async () => {
 
   return (
     <div className="App" >
-    <h1> My Translator </h1>
-    <button onClick={handleLoadUserData}>Login</button>
-    <p>{users.username}</p>
-</div>
+      <h1> My Translator </h1>
+
+      <input onChange={handleInput} placeholder="Enter name"></input>
+      <button onClick={handleLoadUserData}>Login</button>
+      <p>{name}</p>
+    </div>
   )
 }
 
