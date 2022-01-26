ebab0f2427a0f059fc4319f4732b550e32f5345b
Kitt Cheung <kitt.cheung@se.experis.com>
2022-01-26 16:34:27 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        h1 > My Translator < /h1> <
-        /div>
+    return ( 
+	<div className = "App" >
+        <h1> My Translator </h1> 
+	</div>
     );
 }
 
