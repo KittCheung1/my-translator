763f3588eb3ad8ea564c5c411bdae6fecbc2f34c
Kitt Cheung <kitt.cheung@se.experis.com>
2022-01-27 16:14:36 +0100

    feat: console log input

--- src/Components/Views/Login.js
+++ src/Components/Views/Login.js
@@ -4,47 +4,59 @@ const Users = () => {
   const [users, setUsers] = useState([]);
   const [name, setName] = useState("")
 
+
   const handleInput = event => {
     setName(event.target.value)
   }
+  
+  function displayUser ()
+  {
+    console.log(name)
+  }
 
-  const usernames = users.map((user) => {
-    if (name === user.username) {
-      //load new route
-      return name
-    }
-    //else return user to login page
-  })
+  // const usernames = users.map((user) => {
+  //   if (name === user.username) {
+  //     //load new route
+  //     return name
+  //   }
+  //   //else return user to login page
+  // })
 
   //   async function getUser(name) {
   //     const response = await fetch("https://trivia-game-noroff-api.herokuapp.com/trivia");
   //     const data = await response.json();
-  //     return data.find(x => x.username === name);
+  //     const result = data.find(x => x.username === name);
+  //     setUsers(result);
+  //     console.log(result)
   // }
 
-  const handleLoadUserData = async () => {
-    const response = await fetch(
-      "https://trivia-game-noroff-api.herokuapp.com/trivia"
-    );
-    const result = await response.json();
-    ;
-    setUsers(result);
-    console.log(result)
-  };
+  // getUser(name)
 
-  // const handleTranslation =
+  // const handleLoadUserData = async () => {
+  //   const response = await fetch(
+  //     "https://trivia-game-noroff-api.herokuapp.com/trivia"
+  //   );
+  //   const result = await response.json();
+  //   ;
+  //   setUsers(result);
+  //   console.log(result)
+  // };
 
   return (
-   
+
     <div className="App" >
       <div>
-       
-        <h1> My Translator </h1>
 
-        <input type="text" value={name} placeholder="Enter name" onInput={handleInput}></input>
-        <button onClick={handleLoadUserData}>Login</button>
-        <p>{usernames}</p>
-    
+        <h1> My Translator </h1>
+        <form>
+          <fieldset >
+            <label>Username</label>
+            <input type="text" value={name} placeholder="Enter name" onInput={handleInput}></input>
+          </fieldset>
+            <button onClick={displayUser}>Continue</button>
+            <p>{name}</p>
+
+        </form>
       </div>
 
     </div>
