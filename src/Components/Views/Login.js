6c4e9c1ab0ae3e2ee33fe498ae8fb4d744b0b79a
Kitt Cheung <kitt.cheung@se.experis.com>
2022-01-27 08:44:55 +0100

    feat: login button fetch user+ display name

--- src/Components/Views/Login.js
+++ src/Components/Views/Login.js
@@ -1,27 +1,34 @@
 import { useState } from "react";
 
 const Users = () => {
-  //const guitars = [];
   const [users, setUsers] = useState([]);
   const [name, setName] = useState("")
-  
+
   const handleInput = event => {
     setName(event.target.value)
   }
 
-  async function getUser(name) {
-    const response = await fetch("https://trivia-game-noroff-api.herokuapp.com/trivia");
-    const data = await response.json();
-    return data.find(x => x.username === name);
-}
+  const usernames = users.map((user) => {
+    if (name == user.username) {
+      return name
+    }
+  })
+
+  //   async function getUser(name) {
+  //     const response = await fetch("https://trivia-game-noroff-api.herokuapp.com/trivia");
+  //     const data = await response.json();
+  //     return data.find(x => x.username === name);
+  // }
 
   const handleLoadUserData = async () => {
     const response = await fetch(
       "https://trivia-game-noroff-api.herokuapp.com/trivia"
     );
     const result = await response.json();
-    console.log(result)
+
+    ;
     setUsers(result);
+    console.log(result)
   };
 
   return (
@@ -30,7 +37,7 @@ const Users = () => {
 
       <input onChange={handleInput} placeholder="Enter name"></input>
       <button onClick={handleLoadUserData}>Login</button>
-      <p>{name}</p>
+      <p>{usernames}</p>
     </div>
   )
 }
